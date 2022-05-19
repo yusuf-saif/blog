598e2de89572f31c0fd383d3daf99db93f386a47
Yusuf <ysaifullah28kbg@gmail.com>
2022-05-19 16:10:00 +0100

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+    <!DOCTYPE html>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="app.css">
+    <body>
+            <article>
+                        <h1><a href="post">
+                    My First Post
+            </a> </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae amet dolorem p
+                    erspiciatis explicabo nam, eveniet ad, voluptas laudantium delectus reprehenderit minus, accusantium corporis eos culpa non repellendus ipsa est
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi eius numquam, fugiat facere, sit maxime vel veritatis sunt voluptatum corporis consectetur aut 
+                    iusto pariatur. Repudiandae neque et illo minus, porro enim ratione non atque, ullam, harum illum dolor 
+                    recusandae? Numquam quae quibusdam quam obcaecati illo ab nam, tenetur vero laboriosam..</p>
+            </article>
+                    <a href="/">Go backl</a>
+            
+    </body>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+<body>
+        <article>
+                    <h1><a href="post">
+                My First Post
+           </a> </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae amet dolorem p
+                erspiciatis explicabo nam, eveniet ad, voluptas laudantium delectus reprehenderit minus, accusantium corporis eos culpa non repellendus ipsa est
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi eius numquam, fugiat facere, sit maxime vel veritatis sunt voluptatum corporis consectetur aut 
+                 iusto pariatur. Repudiandae neque et illo minus, porro enim ratione non atque, ullam, harum illum dolor 
+                recusandae? Numquam quae quibusdam quam obcaecati illo ab nam, tenetur vero laboriosam..</p>
+        </article>
+
+        <article>
+            <h1><a href="post">
+                My Second Post
+           </a> </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae amet dolorem p
+                erspiciatis explicabo nam, eveniet ad, voluptas laudantium delectus reprehenderit minus, accusantium corporis eos culpa non repellendus ipsa est
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi eius numquam, fugiat facere, sit maxime vel veritatis sunt voluptatum corporis consectetur aut 
+                 iusto pariatur. Repudiandae neque et illo minus, porro enim ratione non atque, ullam, harum illum dolor 
+                recusandae? Numquam quae quibusdam quam obcaecati illo ab nam, tenetur vero laboriosam..</p>
+        </article>
+
+        <article>
+            <h1><a href="post">
+                My Third Post
+           </a> </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae amet dolorem p
+                erspiciatis explicabo nam, eveniet ad, voluptas laudantium delectus reprehenderit minus, accusantium corporis eos culpa non repellendus ipsa est
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi eius numquam, fugiat facere, sit maxime vel veritatis sunt voluptatum corporis consectetur aut 
+                 iusto pariatur. Repudiandae neque et illo minus, porro enim ratione non atque, ullam, harum illum dolor 
+                recusandae? Numquam quae quibusdam quam obcaecati illo ab nam, tenetur vero laboriosam..</p>
+        </article>
+
+
+</body>
